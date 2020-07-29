bcf6d0bd2a5e4152bb0a15f0ef5c92bfb6612eee
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-30 02:16:17 +0700

    header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+          className="app__headerImage"
+        />
+      </div>
+      <h1>Header</h1>
+
+      {/* {Posts} */}
+      {/* {Posts} */}
+    </div>
+  );
 }
 
 export default App;
