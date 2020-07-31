05ab42032c3bd0d0e5fa111a70c25a4f84debc65
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-31 09:54:35 +0700

    SignUp component

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,90 @@
+import { Modal, Button, Input } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import "./SignUp.css";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const SignUp = (props) => {
+  const [open, setOpen] = useState(props.open);
+  const [modalStyle] = useState(getModalStyle);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const classes = useStyles();
+  const handleModal = () => {
+    setOpen(!open);
+  };
+
+  const signUp = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <React.Fragment>
+      <Modal
+        open={open}
+        onClose={handleModal}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="form__signUp">
+            <center>
+              <img
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+                className="app__headerImage"
+              />
+            </center>
+            <Input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign up</Button>
+          </form>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default SignUp;
